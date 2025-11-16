68ddf7e5a4cad7a0cae0b6838b8e5edc8a41bcf9
nylan <nylan@nylan.cat>
2025-11-16 17:14:10 -0500

    fixing click events

--- assets/components/Link.svelte
+++ assets/components/Link.svelte
@@ -7,11 +7,21 @@ let el;
 export {className as class};
 export let href = '/';
 export let style = '';
+export let replace = false;
 
 export const focus = () => el.focus();
 
 $: absoluteHref = href.indexOf('/') === 0 ? $route.basePath + href : href;
 $: hasPath = $route.basePath + $route.path === absoluteHref.replace(/(#|\?).*/, '');
+
+function onClick(event) {
+  if (!route.enabled) return;
+  if (event.defaultPrevented) return;
+  if (event.metaKey || event.ctrlKey || event.shiftKey || event.button !== 0) return;
+  if (href.indexOf('/') !== 0) return;
+  event.preventDefault();
+  route.go(href, {replace});
+}
 </script>
 
-<a href="{absoluteHref}" class="{className}" class:has-path="{hasPath}" style="{style}" bind:this="{el}" on:click><slot/></a>
+<a href="{absoluteHref}" class="{className}" class:has-path="{hasPath}" style="{style}" bind:this="{el}" on:click="{onClick}"><slot/></a>
